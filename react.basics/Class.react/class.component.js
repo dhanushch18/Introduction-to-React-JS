129fc06b42bdd051da1b487733724076ef018ac5
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-02-02 20:10:41 +0530

    Create class.component.js

---
+++ react.basics/Class.react/class.component.js
@@ -0,0 +1,22 @@
+
+/*Create a Class Component
+When creating a React component, the component's name must start with an upper case letter.
+
+The component has to include the extends React.Component statement, this statement creates an inheritance to React.Component, and gives your component access to React.Component's functions.
+
+The component also requires a render() method, this method returns HTML.
+
+*/
+
+/*
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Car /> );
+
+ */ 
