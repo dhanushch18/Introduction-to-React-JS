3cc118d30ae868854cace6e4493c4326ce3e5ca6
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-02-03 10:53:57 +0530

    Create Conditional2.Logical.js

---
+++ react.basics/Conditions.react/Conditional2.Logical.js
@@ -0,0 +1,26 @@
+
+/* Logical && Operator
+Another way to conditionally render a React component is by using the && operator.
+
+*/
+
+
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 &&
+        <h2>
+          You have {cars.length} cars in your garage.
+        </h2>
+      }
+    </>
+  );
+}
+
+const cars = ['Ford', 'BMW', 'Audi'];
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage cars={cars} />);
+
+
