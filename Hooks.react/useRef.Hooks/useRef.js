403a97b918abb2c215fa3c4569ecc03c79310e42
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-02-09 09:09:05 +0530

    Create useRef.js

---
+++ Hooks.react/useRef.Hooks/useRef.js
@@ -0,0 +1,39 @@
+
+/*
+
+React useRef Hook
+
+The useRef Hook allows you to persist values between renders.
+
+It can be used to store a mutable value that does not cause a re-render when updated.
+
+It can be used to access a DOM element directly.
+
+*/
+
+import { useState, useEffect, useRef } from "react";
+
+function Apps() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current +1 ;
+
+  });
+
+
+return (
+  <>
+  <input 
+  type='text' 
+  value={inputValue}
+  onChange={(e) => setInputValue(e.target.value)}/>
+  <h1>Render Count: {count.current}</h1>
+  </>
+
+);
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Apps />);
